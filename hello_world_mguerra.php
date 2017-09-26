83be000e0ba479cbc0511c13e138c9ea23bac7cf
marc98sjd <mguerraexposito@iesesteveterradas.cat>
2017-09-26 15:57:40 +0200

    He afegit la pràctica hello_world

---
+++ hello_world_mguerra.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pràctica HELLO WORLD</title>
+</head>
+
+<body>
+	<?php
+		echo '<h1>esto es un echo</h1>'
+	?>
+	<p>
+		Benvinguts al curs	 de PHP.  
+	</p>
+</body>
+</html>
